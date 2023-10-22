f1229a37378f80e5f474c22b2200b3475dad81d2
Syafiq Asshidqi <75901459+asshdqi05@users.noreply.github.com>
2023-10-22 19:16:58 +0700

    update runLogin

--- src/test/java/saucedemo_test/cucumber/runner/runLogin.java
+++ src/test/java/saucedemo_test/cucumber/runner/runLogin.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/saucedemo_test/cucumber/features",
+        features = "src/test/java/saucedemo_test/cucumber/features/login.feature",
         glue = "saucedemo_test.cucumber.stepDef",
-        plugin = {"html:target/HTML_report.html"}
+        plugin = {"html:target/login_report.html"}
 )
 
 
